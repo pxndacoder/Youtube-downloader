df5db9272ff9a2409e2a08b9c27b2631cb7d4888
Kye Brathwaite <Brathwaitekye@gmail.com>
2026-01-11 21:28:15 -0400

    bind to all interfaces

--- app.py
+++ app.py
@@ -71,6 +71,5 @@ def video():
 def download(filename):
     return send_from_directory(DOWNLOAD_FOLDER, filename, as_attachment=True)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
